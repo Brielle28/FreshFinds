8c09dd271f5980aa61490a03bb3344c03700e2e1
Brielle28 <chukwuemerieclara@gmail.com>
2024-10-15 17:34:37 -0700

    responsive: liked page responsive

--- src/Components/LikeItems/LikeItemsHeader.jsx
+++ src/Components/LikeItems/LikeItemsHeader.jsx
@@ -1,17 +1,15 @@
 import { FaHeart } from "react-icons/fa";
 
 const LikeItemsHeader = () => {
-
   return (
     <>
-      <div className="w-full flex flex-col items-center justify-center md:pt-10 md:px-20 pl-4 sticky top-0 z-10 bg-white pb-5">
+      <div className="w-full flex flex-col items-center justify-center pt-4 md:pt-10 md:px-20 px-4 sticky top-0 z-10 bg-white pb-5">
         <div className="flex items-center justify-start w-full gap-5">
           <div className="flex items-center justify-center gap-2">
-            <h1 className="text-black font-bold text-sm md:text-base lg:text-[35px]">
-              {" "}
+            <h1 className="text-black font-bold text-xl sm:text-2xl md:text-3xl lg:text-[35px]">
               Liked Items
             </h1>
-            <FaHeart className="text-[30px] text-green-500 mt-[7px]" />
+            <FaHeart className="text-xl sm:text-2xl md:text-3xl lg:text-[35px] text-green-500 mt-1 md:mt-2" />
           </div>
         </div>
       </div>
